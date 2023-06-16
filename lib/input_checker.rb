7ab711111fd13cd161fd8d958d626075d87b24d6
T.J. Mercer <ramblingwanunig@gmail.com>
2023-06-16 11:43:08 -0400

    Moved letter check to separate method

--- lib/input_checker.rb
+++ lib/input_checker.rb
@@ -5,11 +5,11 @@ require 'pry'
 
 class InputChecker
   def initialize(correct_word:)
-    @correct_word = correct_word
     @alphabet_colors = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+    @correct_letters = correct_word.split('')
   end
 
-  attr_reader :alphabet_colors
+  attr_reader :alphabet_colors, :correct_letters
 
   def valid_guess(attempt:)
     return 'Words must be five letters long.' unless attempt.length == 5
@@ -27,23 +27,27 @@ class InputChecker
   def compare_guess_to_correct_word(guess:)
     comparison = []
     guess_letters = guess.split('')
-    correct_letters = @correct_word.split('')
     guess_letters.each_with_index do |letter, index|
-      if letter == correct_letters[index]
-        comparison.push('3')
-        correct_letters[index] = '-'
-        assign_alphabet_colors(letter: letter, color: '3')
-      elsif !correct_letters.index(letter).nil? && guess_letters[correct_letters.index(letter)] != letter
-        comparison.push('2')
-        assign_alphabet_colors(letter: letter, color: '2')
-        correct_letters[correct_letters.index(letter)] = '-'
-      else
-        assign_alphabet_colors(letter: letter, color: '1')
-        comparison.push('x')
-      end
+      color = mark_checked_letters(letter: letter, index: index)
+      comparison.push(color)
     end
 
     comparison
+  end
+end
+
+def mark_checked_letters(letter: string, index: number, guess_letters:%w[])
+  if letter == correct_letters[index]
+    correct_letters[index] = '-'
+    assign_alphabet_colors(letter: letter, color: '3')
+    '3'
+  elsif !correct_letters.index(letter).nil? && guess_letters[correct_letters.index(letter)] != letter
+    assign_alphabet_colors(letter: letter, color: '2')
+    correct_letters[correct_letters.index(letter)] = '-'
+    '2'
+  else
+    assign_alphabet_colors(letter: letter, color: '1')
+    'x'
   end
 end
 
